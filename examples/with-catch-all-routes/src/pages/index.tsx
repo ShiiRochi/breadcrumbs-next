1f1cbfd7f448f2569064dce04c2909eb176c981c
Denis Bogdanenko <shiirochi@yandex.ru>
2023-03-25 18:41:20 +0300

    chore: add catch-all example scaffold

---
+++ examples/with-catch-all-routes/src/pages/index.tsx
@@ -0,0 +1,14 @@
+import { NextSeo } from 'next-seo';
+
+export default function Home() {
+  return (
+    <div>
+      <NextSeo
+        title="Next Breadcrumbs | With Catch All Routes"
+        description="The example of NextBreadcrumbs usage with catch all routes"
+      />
+      <main>
+      </main>
+    </div>
+  );
+}
---
+++ examples/with-catch-all-routes/src/pages/items/[[...slug]].js
@@ -0,0 +1,3 @@
+export default function ItemsSlug () {
+    return "ItemsSlug Path";
+};
