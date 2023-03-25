4f1a22c59270f1d8d123b1f640d982f5d156e1ee
Denis Bogdanenko <shiirochi@yandex.ru>
2023-03-25 23:28:07 +0300

    chore: update examples

--- examples/with-catch-all-routes/src/pages/items/[[...slug]].js
+++ examples/with-catch-all-routes/src/pages/items/[[...slug]].js
@@ -1,11 +1,15 @@
-import NextBreadcrumbs from "breadcrumbs-next";
+import NextBreadcrumbs, { createNextCrumbComponent } from "breadcrumbs-next";
+
+const Crumb = createNextCrumbComponent(({ children }) => {
+    return <span>{children}</span>;
+});
 
 export default function ItemsSlug () {
     return (
         <>
             <NextBreadcrumbs
                 Container="nav"
-                Crumb="div"
+                Crumb={Crumb}
             />
             ItemsSlug Path
         </>
