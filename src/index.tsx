58daa0460ed77776349b70f1610d6805ba5efd17
Denis Bogdanenko <shiirochi@yandex.ru>
2023-03-25 23:27:46 +0300

    feat: make text getters more advanced

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,12 @@ import { FC, ReactNode, useEffect, useMemo, useState } from 'react';
 import { useRouter } from 'next/router';
 import {
   _defaultGetDefaultTextGenerator,
-  _defaultGetTextGenerator, flexRender,
+  _defaultGetTextGenerator,
+  flexRender,
   generatePathParts,
   GetDefaultTextGenerator,
   GetTextGenerator,
+  isCatchAllPatternNestedRoute,
 } from './utils';
 import { BreadCrumb } from './types';
 
@@ -61,31 +63,66 @@ export default function NextBreadcrumbs({
   const router = useRouter();
 
   const breadcrumbs: BreadCrumb[] = useMemo(() => {
-    const asPathNestedRoutes = generatePathParts(router.asPath);
-    const pathnameNestedRoutes = generatePathParts(router.pathname);
+    // Здесь будет уже конкретная ссылка
+    const asPathSplit = generatePathParts(router.asPath);
+
+    // Здесь будет паттерн маршрута
+    const pathnameSplit = generatePathParts(router.pathname);
 
     // Маршруты на подобии [[...something]] будут передаваться в генераторы как:
     // getDefaultSomething("something", something[0], 0), а именно - название поля, его значение и его порядковое положение в строке соответственно.
     // Модуль не знает в каком порядке появляются параметры у его пользователя (разработчика) в приложении,
     // а потому безопаснее полагать, что последний лучше осознаёт как их назвать на основании порядкового числа.
     // Вопрос только в том, как их удалять? Ну разве что брать всё до, потом после и удалять этот параметр. То есть нужно понимать какое место он занимает и генерировать "УДАЛЯТОР", "ГДЕ УДАЛЯТОР???!!!"
-    const crumbs = asPathNestedRoutes.map((subpath, idx) => {
-      // "[post_id]" --> "post_id"
-      try {
-        const param = pathnameNestedRoutes[idx].replace(/[[\]]/gm, '');
 
-        const href = `/${asPathNestedRoutes.slice(0, idx + 1).join('/')}`;
+    // Next against catch-all inside catch-all, i.e. [[...items]]/vlog/[[...others]].
+    // Failed to reload dynamic routes: Error: Catch-all must be the last part of the URL.
+    // Если idx входит в паттерн, то нужно вызвать метод с передачей как раз-таки названия слага,
+    // значением и индексом, но индексом, который является индексом
+    // самого элемента в последовательности слага. Далее уже можно выдавать индекс общий,
+    // типа в целом какое он место занимает в списке путей.
+    const catchAllRouteIndex = pathnameSplit.findIndex((item) => isCatchAllPatternNestedRoute(item));
+
+    const crumbs = asPathSplit
+      .map((subpath, idx) => {
+        const isCatchAllSubpath = catchAllRouteIndex !== -1 && idx >= catchAllRouteIndex;
+
+        const subpathPathnameItemIndex = isCatchAllSubpath ? catchAllRouteIndex : idx;
+
+        // "[post_id]" --> "post_id"
+        // "[[...items]]" --> "items"
+        const param = pathnameSplit[subpathPathnameItemIndex].replace(/[[\].]*/gm, '');
+
+        const href = `/${asPathSplit.slice(0, idx + 1).join('/')}`;
+
+        console.log({
+          param,
+          href,
+          subpathPathnameItemIndex,
+          isCatchAllSubpath,
+        });
 
         return {
           href,
-          textGenerator: getTextGenerator(param, router.query),
-          text: getDefaultTextGenerator(subpath, href),
+          textGenerator: getTextGenerator({
+            paramName: param,
+            paramValue: subpath,
+            href,
+            query: router.query,
+            paramIndex: idx,
+            paramSlugIndex: idx,
+          }),
+          text: getDefaultTextGenerator({
+            paramValue: subpath,
+            paramName: param,
+            href,
+            query: router.query,
+            paramIndex: idx,
+            paramSlugIndex: idx,
+          }),
         };
-      } catch (e) {
-        console.trace(e);
-        return false;
-      };
-    }).filter(Boolean);
+      })
+      .filter(Boolean);
 
     return [{ href: '/', text: 'Home', textGenerator: null }, ...crumbs];
   }, [router.asPath, router.pathname, router.query, getTextGenerator, getDefaultTextGenerator]);
@@ -96,7 +133,7 @@ export default function NextBreadcrumbs({
         return flexRender(Crumb, {
           ...crumb,
           key: idx,
-          isLast: idx === breadcrumbs.length - 1
+          isLast: idx === breadcrumbs.length - 1,
         });
       })}
     </Container>
--- src/utils.tsx
+++ src/utils.tsx
@@ -1,25 +1,43 @@
 import { NextRouter } from 'next/router';
-import { cloneElement, ComponentType, ReactNode } from "react";
+import { cloneElement, ComponentType, ReactNode } from 'react';
 
-export type GetDefaultTextGenerator = (path: string, href: string) => string;
-export type GetTextGenerator = (param: string, query: NextRouter['query']) => null | Promise<string>;
+export type GetDefaultTextGenerator = (config: {
+  paramName: string;
+  paramValue: string;
+  href: string;
+  // global index within path
+  paramIndex: number;
+  // The index of param under paramName with a given value within
+  // a slug will create its own index counting context
+  paramSlugIndex: number;
+  query: NextRouter['query'];
+}) => string;
+
+export type GetTextGenerator = (config: {
+  paramName: string;
+  paramValue: string;
+  href: string;
+  // global index within path
+  paramIndex: number;
+  // The index of param under paramName with a given value within
+  // a slug will create its own index counting context
+  paramSlugIndex: number;
+  query: NextRouter['query'];
+}) => null | Promise<string>;
 export type GeneratePathParts = (path: string) => string[];
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const _defaultGetTextGenerator: GetTextGenerator = (param, query) => null;
+export const _defaultGetTextGenerator: GetTextGenerator = () => null;
 
-export const _defaultGetDefaultTextGenerator: GetDefaultTextGenerator = (path) => path;
+export const _defaultGetDefaultTextGenerator: GetDefaultTextGenerator = ({ paramValue }) => paramValue;
 
 export const generatePathParts: GeneratePathParts = (path): string[] => {
-    try {
-    return path
-        .replace(/\?.+/gm, '')
-        .split('/')
-        .filter(Boolean);
-    } catch (err) {
-        console.trace(err);
-        return [];
-    }
+  try {
+    return path.replace(/\?.+/gm, '').split('/').filter(Boolean);
+  } catch (err) {
+    console.trace(err);
+    return [];
+  }
 };
 
 /**
@@ -28,48 +46,54 @@ export const generatePathParts: GeneratePathParts = (path): string[] => {
  * Forked from the repo mentioned below:
  * https://github.com/TanStack/table/blob/main/packages/react-table
  */
-export function flexRender<TProps extends object>(Component: Renderable<TProps>, props: TProps): ReactNode | JSX.Element {
-    return isAtomicComponent(Component)
-        ? Component
-        : isReactComponent<TProps>(Component)
-            ? <Component {...props} />
-            : cloneElement(Component as JSX.Element, props as TProps);
+export function flexRender<TProps extends object>(
+  Component: Renderable<TProps>,
+  props: TProps,
+): ReactNode | JSX.Element {
+  return isAtomicComponent(Component) ? (
+    Component
+  ) : isReactComponent<TProps>(Component) ? (
+    <Component {...props} />
+  ) : (
+    cloneElement(Component as JSX.Element, props as TProps)
+  );
 }
 
 function isClassComponent(component: any) {
-    return (
-        typeof component === 'function' &&
-        (() => {
-            const proto = Object.getPrototypeOf(component)
-            return proto.prototype && proto.prototype.isReactComponent
-        })()
-    )
+  return (
+    typeof component === 'function' &&
+    (() => {
+      const proto = Object.getPrototypeOf(component);
+      return proto.prototype && proto.prototype.isReactComponent;
+    })()
+  );
 }
 
 function isExoticComponent(component: any) {
-    return (
-        typeof component === 'object' &&
-        typeof component.$$typeof === 'symbol' &&
-        ['react.memo', 'react.forward_ref', 'react.portal'].includes(component.$$typeof.description)
-    )
+  return (
+    typeof component === 'object' &&
+    typeof component.$$typeof === 'symbol' &&
+    ['react.memo', 'react.forward_ref', 'react.portal'].includes(component.$$typeof.description)
+  );
 }
 
-function isReactComponent<TProps>(
-    component: unknown
-): component is ComponentType<TProps> {
-    return (
-        isClassComponent(component) ||
-        typeof component === 'function' ||
-        isExoticComponent(component)
-    )
+function isReactComponent<TProps>(component: unknown): component is ComponentType<TProps> {
+  return isClassComponent(component) || typeof component === 'function' || isExoticComponent(component);
 }
 
-type AtomicReactNodes = "string" | "number" | "boolean" | null | undefined;
+export type Renderable<TProps> = ReactNode | ComponentType<TProps> | JSX.Element;
 
-function isAtomicComponent (component: unknown): component is AtomicReactNodes {
-    return component === null || ["string", "number", "boolean", "undefined"].includes(typeof component)
-};
+export type AtomicReactNodes = 'string' | 'number' | 'boolean' | null | undefined;
 
+function isAtomicComponent(component: unknown): component is AtomicReactNodes {
+  return component === null || ['string', 'number', 'boolean', 'undefined'].includes(typeof component);
+}
 
-export type Renderable<TProps> = ReactNode | ComponentType<TProps> | JSX.Element
-
+/**
+ * Checks that any given routePath is a catchAll nested route
+ * @param routePath
+ * @example
+ * [[...]] --> false
+ * [[...items]] --> true
+ */
+export const isCatchAllPatternNestedRoute = (routePath: string): boolean => /^\[\[\.\.\..+]]$/.test(routePath);
